3badcb50f135fc679587442205e25b253f56be12
Osman Abdullayev <os.abdullayev@gmail.com>
2022-11-19 12:19:59 +0400

    final

---
+++ README.md
@@ -0,0 +1,3 @@
+## Note App
+
+This Web App allows to create notes with tags and titles. Written with React JS and TypeScript.
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,10 +1,15 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useNote } from "./NoteLayout";
-import ReactMarkDown from "react-markdown";
+import ReactMarkdown from "react-markdown";
 
-export function Note() {
+type NoteProps = {
+	onDelete: (id: string) => void;
+};
+
+export function Note({ onDelete }: NoteProps) {
 	const note = useNote();
+	const navigate = useNavigate();
 
 	return (
 		<>
@@ -14,7 +19,7 @@ export function Note() {
 					{note.tags.length > 0 && (
 						<Stack gap={1} direction="horizontal" className="flex-wrap">
 							{note.tags.map((tag) => (
-								<Badge className="text-truncate fs-6" key={tag.id}>
+								<Badge className="text-truncate" key={tag.id}>
 									{tag.label}
 								</Badge>
 							))}
@@ -24,16 +29,23 @@ export function Note() {
 				<Col xs="auto">
 					<Stack gap={2} direction="horizontal">
 						<Link to={`/${note.id}/edit`}>
-							<Button variant="primary m-0">Edit</Button>
+							<Button variant="primary">Edit</Button>
 						</Link>
-						<Button variant="outline-danger m-0">Delete</Button>
+						<Button
+							onClick={() => {
+								onDelete(note.id);
+								navigate("/");
+							}}
+							variant="outline-danger m-0">
+							Delete
+						</Button>
 						<Link to="/">
 							<Button variant="outline-secondary m-0">Back</Button>
 						</Link>
 					</Stack>
 				</Col>
 			</Row>
-			<ReactMarkDown>{note.markdown}</ReactMarkDown>
+			<ReactMarkdown>{note.markdown}</ReactMarkdown>
 		</>
 	);
 }
